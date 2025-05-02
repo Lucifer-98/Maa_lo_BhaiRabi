2cfc2c20af2abffdc0313738ef09d4c9c2ce8181
HP <HP@DESKTOP-NLEH1OE>
2025-05-02 16:34:57 +0530

    Prabu_dayakara_auvul karibini

---
+++ src/Omm_nama_shivaya/Omm_Hanumantenama.java
@@ -0,0 +1,16 @@
+package Omm_nama_shivaya;
+
+class Mahadev {
+	void show() {
+		System.out.println(
+				"Hai Prabhu Mora sabu Tik Heijau Love relation mo Anwesha ku hni Bhai hue Baba aji jain sabu chadiki neicha kintu ebe nuhe baba Mu paruni Mo anwesha mote hni vala pau au mote hni bha hau au chitting na karumote hni loyal ,commited,au mote hni bha hau,mu jetiki vala pauchi taku ta thu besi mote vala pau au mote hni bha hau prabhu Au Mo Ghara Paristit vala heijau mo didi sasu ghara ku jau rosy didi ra bi love life tik heijau ,mote jaldi job hejau jha padichi sabu mane rahu au confident re kama akare prabhub  job kariki anwesha ebeb  call karu au mu bujile bujiki pura loyal rahu prabhu satat kahuchi kebeb kou jia ku dekhibin jha vul karichi sabu lkaribini hela pare sabu sata jha kahichi karibi");
+	}
+}
+
+public class Omm_Hanumantenama extends Mahadev {
+	public static void main(String[] args) {
+		Omm_Hanumantenama omm = new Omm_Hanumantenama();
+		omm.show();
+	}
+
+}
